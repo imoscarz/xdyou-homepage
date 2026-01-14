7362785083332f71a384a7d328562d30f9862922
imoscarz <imoscarz@icloud.com>
2026-01-14 14:37:29 +0800

    feat: 添加图片优化配置

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
+    formats: ["image/webp"],
+    minimumCacheTTL: 2678400, 
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920], 
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384], 
     remotePatterns: [
       {
         protocol: "https",
